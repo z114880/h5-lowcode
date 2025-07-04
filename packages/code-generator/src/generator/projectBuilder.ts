24ac15519fad1ac4c35d7865a3ee97f639000e3d
Andrew <1148803335@qq.com>
2025-07-04 20:16:56 +0800

    update favicon

--- packages/code-generator/src/generator/projectBuilder.ts
+++ packages/code-generator/src/generator/projectBuilder.ts
@@ -104,7 +104,7 @@ export default class ProjectBuilder {
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
       <title>${this.options.config.title || ''}</title>
-      <link rel="icon" type="image/svg+xml" href="/funet.svg" />
+      <link rel="icon" href="/funet.png" />
       <link rel="stylesheet" href="./normalize.css">
       <link rel="stylesheet" href="./animation.css">
       <link rel="stylesheet" href="./index.css">
